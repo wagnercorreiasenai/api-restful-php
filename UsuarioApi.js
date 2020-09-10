664874ef7f7564f4a1b9147dcbe1b76b16693833
Wagner Correia <43221732+wagnercorreiasenai@users.noreply.github.com>
2020-09-09 21:18:20 -0300

    Update UsuarioApi.js

--- UsuarioApi.js
+++ UsuarioApi.js
@@ -19,6 +19,21 @@ function getUsuarios() {
 function exibirUsuarios(usuarios) {
     var div = document.getElementById("conteudo");
     div.innerHTML = usuarios[0].nome;
+    inserirLinha(usuarios[0]);
+}
+
+function inserirLinha(usuario) {
+    var tabela = document.getElementById("tabelaDados");
+    var numLinhas = tabela.rows.length;
+    //var numCols = tabela.rows[numLinhas - 1].cells.length;
+    var novaLinha = tabela.insertRow(numLinhas);
+
+    var celNome = novaLinha.insertCell(0);
+    celNome.innerHTML = usuario.nome;
+
+    var celEmail = novaLinha.insertCell(1);
+    celEmail.innerHTML = usuario.email;
+
 }
 
 function adicionarUsuarios() {
